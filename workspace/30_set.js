9304715b89d42e33e0fdad34aca1d2f6b6539c42
pinixk <bjkim0110@gmail.com>
2022-05-20 12:19:02 +0900

    5/20

--- workspace/30_set.js
+++ workspace/30_set.js
@@ -1,9 +1,11 @@
 var setA = new Set([1,2,3,4]);
 var setB = new Set([3,4,5,6]);
 
-var intersection = new Set([...setA].filter(x => setB.has(x)));
-console.log(intersection);
-var difference = new Set([...setA].filter(x => !setB.has(x)));
+var intersection = new Set([...setA].filter(x => setB.has(x))); // 교집합
+console.log(`intersection : ${[...intersection].toString()}`);
+
+var difference = new Set([...setA].filter(x => !setB.has(x)));  // 차집합
 console.log("difference :" + [...difference].toString());
-var union = new Set([...setA,...setB])
+
+var union = new Set([...setA,...setB])                          // 합집합
 console.log("union :" + [...union].toString());
