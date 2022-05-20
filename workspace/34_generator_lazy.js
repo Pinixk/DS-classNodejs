c305e4db95578f0df570f42e1f9e19dbb1daf068
pinixk <bjkim0110@gmail.com>
2022-05-20 12:56:47 +0900

    5/20

--- workspace/34_generator_lazy.js
+++ workspace/34_generator_lazy.js
@@ -1,3 +1,13 @@
 const _ = require('fxjs/Strict')
 const L = require('fxjs/Lazy')
 const C = require('fxjs/Concurrency')
+
+const arr = [1,2,3,4,5,6,7,8]
+const result = _.go(            // 즉시실행 pipe함수
+    arr,
+    _.map(arr => arr),          // 새로운 배열 반환
+    _.filter(arr => arr % 2),   // 홀수만 거름
+    _.take(2)                   // 2개만 출력
+)
+console.log(result);            // [1, 3]
+console.log([...result]);
