cfba61b05925508057e26c4900eb24438bd39dfd
pinixk <bjkim0110@gmail.com>
2022-05-23 17:51:06 +0900

    5/23

--- workspace/lesson3_3/dist/img_lazy.js
+++ workspace/lesson3_3/dist/img_lazy.js
@@ -1,22 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"))
+    // [].slice.call : 유사배열이나 iterable object를 배열로 만듦
     let active = false
     const lazyLoad = function () {
         if (active === false) {
             active = true
             setTimeout(() => {
                 lazyImages = lazyImages.map((lazyImage) => {
-                    if (lazyImage.getBoundingClientRect().top <= window.innerHeight &&
-                        window.getComputedStyle(lazyImage).display !== 'none') {
+                    if (lazyImage.getBoundingClientRect().top <= window.innerHeight &&  
+                        // getBoundingClientRect : el의 크기와 뷰포트에 상대적인 위치 정보를 제공
+                        // window.innerHeight는 웹브라우저를 제외한 다큐먼트의 높이
+                        window.getComputedStyle(lazyImage).display !== 'none') {        
+                        // getComputedStyle : 인자로 전달받은 요소의 모든 CSS 속성값을 담은 객체를 회신
                         lazyImage.src = lazyImage.dataset.src
                         lazyImage.classList.remove('lazy')
+                        lazyImage.classList.add("fadein")
                         return null
                     } else return lazyImage
                 }).filter(image => image)
+                // !lazyImages.length의 값이 0일 경우 이벤트 추가
                 if (!lazyImages.length) {
                     document.removeEventListener("scroll", lazyLoad)
                 } else active = false
-            }, 800)
+            }, 200)
         }
     }
     document.addEventListener("scroll", lazyLoad)
