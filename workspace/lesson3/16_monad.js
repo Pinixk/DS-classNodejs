1d6465202267767b6407ed72578db4a388a0a0f1
pinixk <bjkim0110@gmail.com>
2022-05-24 16:35:07 +0900

    05/24

--- workspace/lesson3/16_monad.js
+++ workspace/lesson3/16_monad.js
@@ -5,5 +5,5 @@ const log = x => console.log(x);
 // new Promise를 통해 Promise<Pending> 같은 타입으로 반환
 // resolve로 unit값을 반환
 // .then로 순수한 값을 도출
-fg('{"temp":36.5}').catch(_ => 'JSON PARSE IS NOT WORKING').then(log)
+fg('{"temp":36.5}').catch(_ => 'JSON PARSE IS NOT WORKING').then(log) // 숫자로만 시작하지 않으면 변수명으로 _, $ 등도 지정 가능하다
 // {"temp":36.5} -> json object화 -> temp값(36.5) 도출
