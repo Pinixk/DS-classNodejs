9e166169b961c8d843e0a4f1b821322279356493
pinixk <bjkim0110@gmail.com>
2022-05-26 17:25:17 +0900

    05/26

--- workspace/lesson4/06_get_static.js
+++ workspace/lesson4/06_get_static.js
@@ -5,6 +5,7 @@ const loogger = require('morgan')
 const app = express()
 const PORT = 3000
 const _path = path.join(__dirname, './dist')
+const fs = require('fs')
 console.log(_path);
 app.use('/dist', express.static(_path))
 app.use(loogger('tiny'))
@@ -17,6 +18,12 @@ app.use((req,res,next)=>{
 app.get('/book/:bookName',(req,res)=>{
     const {bookName} = req.params
     res.send(`안녕하세요 알라딘 입니다. ${bookName}을 주문하셨습니다.`)
+})
+app.get('/join', (req,res) => {
+    fs.readFile('./dist/join.html', function(error, data){
+        res.writeHead(200, {'Content-Type' : 'text/html'})        
+        res.end(data)
+    })
 })
 app.get("/", simple_module.index)
 app.get('/users/:userName/books/:bookName', simple_module.handleBook) // : 변수처럼 받아 쓸 수 있음
