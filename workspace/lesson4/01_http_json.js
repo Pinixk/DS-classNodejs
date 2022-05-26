76315cd757cf4f191d0e768dbd5ed05cdfc3ef8e
pinixk <bjkim0110@gmail.com>
2022-05-26 09:23:16 +0900

    05/26

--- workspace/lesson4/01_http_json.js
+++ workspace/lesson4/01_http_json.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const PORT = 12010
 const server = http.createServer((req, res)=>{
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
     const obj = {'이름': "KBJ"}
     res.end(JSON.stringify(obj))
 })
