8a114dbeeff7f5b9581573d6844070d98b9866b8
pinixk <bjkim0110@gmail.com>
2022-05-26 11:58:35 +0900

    05/26

--- workspace/lesson4/03_express.js
+++ workspace/lesson4/03_express.js
@@ -1,10 +1,14 @@
 const express = require("express")
+const path = require('path')
 const app = express()
-app.set('port', process.env.PORT || 3000)
 
+app.set('port', process.env.PORT || 3000)
+app.use(express.static('public'))
 app.get("/", (req, res) => {
-    res.send('Hello express')
+    console.log(__dirname);
+    res.send(path.join(__dirname,'/public/index.html'))
 })
+
 const PORT = app.get('port')
 app.listen(PORT, () => {
     console.log(`Server Running at http://127.0.0.1:${PORT}/`);
