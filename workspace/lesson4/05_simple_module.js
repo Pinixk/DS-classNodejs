872979a7aeebbb9b02fd71be29d1d1b3a3897f0a
pinixk <bjkim0110@gmail.com>
2022-05-26 16:49:52 +0900

    05/26

--- workspace/lesson4/05_simple_module.js
+++ workspace/lesson4/05_simple_module.js
@@ -1,7 +1,15 @@
+var fs = require('fs')
+
 module.exports.intro = (req,res) => {
     res.send('<h1>Express Routing Practice</h1>')
 }
 module.exports.handleBook = (req,res) => {
     console.log(req.params);
     res.send(req.params)
+}
+module.exports.index = (req,res) => {
+    fs.readFile('./dist/index.html', function(error.data){
+        res.writeHead(200, {'Content-Type' : 'text/html'})        
+        res.end(data)
+    })
 }
