86dee0bb8762d8ece4163c14ffeff8f187ae834c
Pinixk <bjkim0110@gmail.com>
2022-05-30 14:46:50 +0900

    05/30

--- express-gen/routes/users.js
+++ express-gen/routes/users.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
    res.render('users/userList', { title: 'Express' });
 });
 router.get('/join', function(req, res, next) {
-   res.render('users/join', { title: 'Express' });
+   res.render('users/join', { title: 'Express', data:'KBJ'});
 });
 router.get('/login', function(req, res, next) {
    res.render('users/login', { title: 'Express' });
