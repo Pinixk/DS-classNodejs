16425b22d8921ab83a072f0b294e83d63dafaca3
Pinixk <bjkim0110@gmail.com>
2022-05-31 11:55:06 +0900

    05/31

--- MEVN-master/6장/README.md
+++
@@ -1,2 +0,0 @@
-# 6장 MongoDB의 기본과 성능테스트
-`npm install` 후 `mongoDB`를 구동 시킨 후 테스팅하시면 됩니다.
--- MEVN-master/9장/sensor-frontend/src/plugins/socketPlug.js
+++
@@ -1,8 +0,0 @@
-import io from 'socket.io-client'; 
-export default {
-    install: (app, {host}) => {
-      const socketClient = io(`${host}`);
-      app.config.globalProperties.$socket = socketClient
-      app.provide('socket', socketClient)
-    }
-  }
